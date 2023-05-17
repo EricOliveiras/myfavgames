3c5a5b9c686011c283d25772a49c4a7c0d573092
Eric Oliveira <oiericoi@hotmail.com>
2023-05-17 15:49:02 -0300

    feat: add index file

---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+import 'express-async-errors';
+
+import { app } from './config/server';
+import { port } from './config/vars';
+
+app.listen(port, () => console.log(`Server is running in port: ${port}`));
