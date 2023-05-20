30d83b496fce720ef0fc2af86a1f929ea80e1908
Eric Oliveira <oiericoi@hotmail.com>
2023-05-20 18:27:03 -0300

    feat: add update service

---
+++ backend/src/modules/user/services/UpdateUser.ts
@@ -0,0 +1,26 @@
+import { UserRequest, UserResponse } from '../interfaces/User';
+import { UserRepository } from '../repository/UserRepository';
+
+export class UpdateUser {
+  constructor(private userRepository: UserRepository) {}
+
+  public async execute(id: string, { username, email, password, imageUrl }: UserRequest): Promise<UserResponse> {
+    const getUserById = await this.userRepository.getOne(id);
+    const getUserByUsername = await this.userRepository.getByUsername(username);
+
+    if (!getUserById) {
+      throw new Error('User not found.');
+    }
+
+    if (getUserByUsername && getUserByUsername.id !== id) {
+      throw new Error('Username already registered.');
+    }
+
+    return await this.userRepository.update(id, {
+      username,
+      email,
+      password,
+      imageUrl
+    });
+  }
+}
