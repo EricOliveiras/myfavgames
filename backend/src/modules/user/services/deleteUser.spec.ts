2e4c21028d5198005107ecc81548d0941bf51214
Eric Oliveira <oiericoi@hotmail.com>
2023-05-21 12:21:59 -0300

    test: add delete user test

---
+++ backend/src/modules/user/services/deleteUser.spec.ts
@@ -0,0 +1,50 @@
+import { UserRepository } from '../repository/UserRepository';
+import { createFakerUser } from '../../../utils/fakerUser';
+import { prisma } from '../../../database/prisma';
+import { UserRequest } from '../interfaces/User';
+import { CreateUser } from './CreateUser';
+import { DeleteUser } from './DeleteUser';
+import { GetUser } from './GetUser';
+import { randomUUID } from 'crypto';
+
+describe('Delete user', () => {
+  let userRepository: UserRepository;
+  let createUserService: CreateUser;
+  let deleteUserService: DeleteUser;
+  let getUserService: GetUser;
+  let fakerUser: UserRequest;
+
+  beforeAll(async () => {
+    userRepository = new UserRepository();
+    createUserService = new CreateUser(userRepository);
+    deleteUserService = new DeleteUser(userRepository);
+    getUserService = new GetUser(userRepository);
+    fakerUser = createFakerUser();
+    await prisma.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prisma.user.deleteMany();
+    await prisma.$disconnect();
+  });
+
+  it('should delete a user', async () => {
+    const user: UserRequest = fakerUser;
+
+    const createUser = await createUserService.execute(user);
+
+    await deleteUserService.execute(createUser.id);
+
+    await expect(getUserService.execute(createUser.id))
+      .resolves
+      .toBe(null);
+  });
+
+  it('should return an error if user not found', async () => {
+    const id = randomUUID();
+
+    await expect(deleteUserService.execute(id))
+      .rejects
+      .toThrowError('User not found.');
+  });
+});
