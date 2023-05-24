78894963b132de6ff41390d722f37d142961e33b
Eric Oliveira <oiericoi@hotmail.com>
2023-05-24 12:11:28 -0300

    refactor: modify faker user util

--- backend/src/utils/fakerUser.ts
+++ backend/src/utils/fakerUser.ts
@@ -3,8 +3,8 @@ import { faker } from '@faker-js/faker';
 import { UserRequest } from '../modules/user/interfaces/User';
 
 export const createFakerUser = (): UserRequest => {
-  const username = faker.person.firstName() + faker.person.lastName();
-  const password = faker.internet.password();
+  const username = faker.person.firstName().toLowerCase();
+  const password = faker.internet.password().toLowerCase();
   const email = faker.internet.email({ firstName: username.toLowerCase() });
 
   return {
