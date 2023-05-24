710d002fb59aeba136769f43039fb1539b12e414
Eric Oliveira <oiericoi@hotmail.com>
2023-05-24 12:15:52 -0300

    refactor: modify create user service

--- backend/src/modules/user/services/CreateUser.ts
+++ backend/src/modules/user/services/CreateUser.ts
@@ -2,22 +2,23 @@ import { User } from '@prisma/client';
 import { hash } from 'bcrypt';
 
 import { IUserRepository } from '../interfaces/IUserRepository';
-import { UserRequest } from '../interfaces/User';
+import { HttpException } from '../../../errors/HttpException';
 import { saltRounds } from '../../../config/vars';
+import { UserRequest } from '../interfaces/User';
 
 export class CreateUser {
   constructor(private userRepository: IUserRepository) {}
 
   public async execute({ username, email, password, imageUrl }: UserRequest): Promise<User> {
-    const checkIfUsernameExist = await this.userRepository.getByUsername(username);
-    const checkIfEmailExist = await this.userRepository.getByEmail(email);
+    const checkIfUsernameExist = !!await this.userRepository.getByUsername(username);
+    const checkIfEmailExist = !!await this.userRepository.getByEmail(email);
 
     if (checkIfUsernameExist) {
-      throw new Error('Username already registered.');
+      throw new HttpException(409, 'Username already registered.');
     }
 
     if (checkIfEmailExist) {
-      throw new Error('Email already registered.');
+      throw new HttpException(409, 'Email already registered.');
     }
 
     const hashPassword = await hash(password, saltRounds);
