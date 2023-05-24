89512b386f9c68ff216fc5a27e4527d6193bc8c3
Eric Oliveira <oiericoi@hotmail.com>
2023-05-24 12:21:06 -0300

    refactor: modify delete user service

--- backend/src/modules/user/services/DeleteUser.ts
+++ backend/src/modules/user/services/DeleteUser.ts
@@ -1,13 +1,14 @@
+import { HttpException } from '../../../errors/HttpException';
 import { UserRepository } from '../repository/UserRepository';
 
 export class DeleteUser {
   constructor(private userRepository: UserRepository) {}
 
   public async execute(id: string): Promise<void> {
-    const getUser = await this.userRepository.getOne(id);
+    const getUser = !!await this.userRepository.getOne(id);
 
     if (!getUser) {
-      throw new Error('User not found.');
+      throw new HttpException(404, 'User not found.');
     }
 
     await this.userRepository.delete(id);
