5660860f7c0e419e932a2e2e4c8500817fcdd6fd
Eric Oliveira <oiericoi@hotmail.com>
2023-05-25 14:07:23 -0300

    feat: add get by id and get by usename methods in user routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { CreateUserController } from '../modules/user/controller/CreateUserController';
+import { GetUserController } from '../modules/user/controller/GetUserController';
+import { GetUserByUsernameController } from '../modules/user/controller/GetUserByUsernameController';
 
 export const userRouter = Router();
 
 userRouter.post('/', CreateUserController.handle);
+userRouter.get('/:id', GetUserController.handle);
+userRouter.get('/username/:username', GetUserByUsernameController.handle);
