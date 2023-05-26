20540abb2fa3e78615df5780c74f0ab1bf13482e
Eric Oliveira <oiericoi@hotmail.com>
2023-05-26 13:19:43 -0300

    feat: add delete user route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { CreateUserController } from '../modules/user/controller/CreateUserController';
 import { GetUserController } from '../modules/user/controller/GetUserController';
 import { GetUserByUsernameController } from '../modules/user/controller/GetUserByUsernameController';
+import { DeleteUserController } from '../modules/user/controller/DeleteUserController';
 
 export const userRouter = Router();
 
 userRouter.post('/', CreateUserController.handle);
 userRouter.get('/:id', GetUserController.handle);
 userRouter.get('/username/:username', GetUserByUsernameController.handle);
+userRouter.delete('/:id', DeleteUserController.handle);
