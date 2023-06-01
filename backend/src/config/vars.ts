506edc0a24a9e00b6efdb9eaaecf2671689bb307
Eric Oliveira <oiericoi@hotmail.co>
2023-06-01 17:23:48 -0300

    refactor: add jwt secret vars

--- backend/src/config/vars.ts
+++ backend/src/config/vars.ts
@@ -5,8 +5,10 @@ dotenv.config();
 const port = <string>process.env.PORT || '5555';
 const saltRoundsEnv = <string>process.env.SALT_ROUNDS;
 const saltRounds = parseInt(saltRoundsEnv);
+const jwtSecret = <string>process.env.JTW_SECRET;
 
 export {
   port,
-  saltRounds
+  saltRounds,
+  jwtSecret
 };
