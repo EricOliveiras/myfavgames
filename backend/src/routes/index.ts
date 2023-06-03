3554f742c264d87c355ef4375be57b55b533aecb
Eric Oliveira <oiericoi@hotmail.co>
2023-06-03 14:55:38 -0300

    feat: add auth routes in index routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { rootRouter } from './root.routes';
 import { userRouter } from './user.routes';
+import { authRouter } from './auth.routes';
 
 export const routes = Router();
 
+routes.use('/auth', authRouter);
 routes.use('/users', userRouter);
 routes.use('/', rootRouter);
